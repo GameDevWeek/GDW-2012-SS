5ad92c58591db1600e7d3ac222cb8ecd9b440619
Lusito <core@roughael.net>
2015-01-02 23:08:29 +0100

    Help Screens from json instead of fixed image

--- gdx-sotf/src/main/java/de/hochschuletrier/gdw/ss12/menu/MenuPageHelp.java
+++ gdx-sotf/src/main/java/de/hochschuletrier/gdw/ss12/menu/MenuPageHelp.java
@@ -12,8 +12,8 @@ public class MenuPageHelp extends MenuPage {
         int i = 0;
         int y = 370;
         int yStep = 55;
-        addPageEntry(menuManager, x, y - yStep * (i++), "Spiel und Ziel", new MenuPageHelpImageScroller(skin, menuManager, "menu_help_goal"));
-        addPageEntry(menuManager, x, y - yStep * (i++), "Items und Erklärung", new MenuPageHelpImageScroller(skin, menuManager, "menu_help_legend"));
+        addPageEntry(menuManager, x, y - yStep * (i++), "Spiel und Ziel", new MenuPageHelpImageScroller(skin, menuManager, "data/json/help_goal.json"));
+        addPageEntry(menuManager, x, y - yStep * (i++), "Items und Erklärung", new MenuPageHelpImageScroller(skin, menuManager, "data/json/help_legend.json"));
 
         addCenteredButton(menuManager.getWidth() - 100, 54, 100, 40, "Zurück", () -> menuManager.popPage());
     }
--- gdx-sotf/src/main/java/de/hochschuletrier/gdw/ss12/menu/MenuPageHelpImageScroller.java
+++ gdx-sotf/src/main/java/de/hochschuletrier/gdw/ss12/menu/MenuPageHelpImageScroller.java
@@ -1,20 +1,66 @@
 package de.hochschuletrier.gdw.ss12.menu;
 
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.ScrollPane;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.utils.Align;
 import de.hochschuletrier.gdw.commons.gdx.menu.MenuManager;
 import de.hochschuletrier.gdw.commons.gdx.menu.widgets.DecoImage;
+import de.hochschuletrier.gdw.commons.jackson.JacksonReader;
+import java.util.List;
+import java.util.Map;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class MenuPageHelpImageScroller extends MenuPage {
 
-    public MenuPageHelpImageScroller(Skin skin, MenuManager menuManager, String image) {
-        super(skin, "menu_bg_help_detail");
-        ScrollPane scrollPane = new ScrollPane(new DecoImage(assetManager.getTexture(image)), skin, "transparent");
-        scrollPane.setFadeScrollBars(false);
-        scrollPane.setFlickScroll(true);
-        scrollPane.setBounds(70, 90, 900, 520);
+    private static final Logger logger = LoggerFactory.getLogger(MenuPageHelpImageScroller.class);
 
-        addActor(scrollPane);
+    public MenuPageHelpImageScroller(Skin skin, MenuManager menuManager, String jsonFile) {
+        super(skin, "menu_bg_help_detail");
+
+        try {
+            List<String> list = JacksonReader.readList(jsonFile, String.class);
+
+            Table table = new Table();
+            table.setWidth(850);
+            for (String text : list) {
+                String[] split = text.split(":", 2);
+                if (split.length == 2) {
+                    switch (split[0]) {
+                        case "images":
+                            table.row();
+                            table.add(new DecoImage(assetManager.getTexture(split[1]))).align(Align.left);
+                            break;
+                        case "title":
+                            table.row().fillX().expandX();
+                            Label title = new Label(split[1], skin, "helpTitle");
+                            title.setWrap(true);
+                            title.setAlignment(Align.left);
+                            table.add(title);
+                            break;
+                        default:
+                            table.row().fillX().expandX();
+                            Label label = new Label(split[1], skin, "helpText");
+                            label.setWrap(true);
+                            label.setAlignment(Align.left);
+                            table.add(label);
+                            break;
+                    }
+                }
+            }
+
+            ScrollPane scrollPane = new ScrollPane(table, skin, "transparent");
+            scrollPane.setFadeScrollBars(false);
+            scrollPane.setFlickScroll(true);
+            scrollPane.setBounds(70, 90, 900, 520);
+
+            addActor(scrollPane);
+        } catch (Exception e) {
+            logger.error("Error reading help json", e);
+        }
+
         addCenteredButton(menuManager.getWidth() - 100, 54, 100, 40, "Zurück", () -> menuManager.popPage());
     }
 }
